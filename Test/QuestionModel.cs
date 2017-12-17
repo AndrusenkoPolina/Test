9a576381b6e63cfabad9879b549721446ca2d429
Полина <@LENOVO-PC>
2017-12-17 12:55:32 +0300

    Отображение из БД использую MVVM

---
+++ Test/QuestionModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class QuestionModel
+    {
+        public int id { get; set; }
+        public string question { get; set; }
+        public string answerA { get; set; }
+        public string answerB { get; set; }
+
+        public static List<QuestionModel>GetQuestion()
+        {
+            List<QuestionModel> question = new List<QuestionModel>();
+            using (SqlConnection connection = new SqlConnection("Server=LENOVO-PC\\POLINA;Database=Question;Trusted_Connection=True;"))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("select q.id_question, question,answerA,answerB from question q inner join answerAB a ON a.id_question = q.id_question", connection);
+                var questionReader = command.ExecuteReader();
+                while (questionReader.Read())
+                {
+
+                    QuestionModel q = new QuestionModel();
+                    q.id = (int) questionReader["id_question"];
+                    q.question = (string)questionReader["question"];
+                    q.answerA = (string)questionReader["answerA"];
+                    q.answerB = (string)questionReader["answerB"];
+
+                    question.Add(q);
+                }
+            }
+            return question;            
+        }
+    }
+}
---
+++ Test/QuestionsViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.IO;
+
+namespace Test
+{
+     public class QuestionsViewModel: INotifyPropertyChanged
+    
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void RaisePropertyChanged(String propertyName)
+        {
+            var ev = PropertyChanged;
+            if (ev != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public ObservableCollection<AnswerViewModel> Questions { get; set; }
+
+        public QuestionsViewModel()
+        {
+            Questions = new ObservableCollection<AnswerViewModel>();
+            var question = QuestionModel.GetQuestion();
+
+            foreach (var q in question)
+            {
+                Questions.Add(new AnswerViewModel()
+                {
+                    id = q.id,
+                    question = q.question,
+                    answerA=q.answerA,
+                    answerB=q.answerB
+
+                });
+            }
+        }
+       
+    }
+}
